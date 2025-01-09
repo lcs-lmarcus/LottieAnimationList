f5c78c0fdf548be0b89edf0ae226acc66799b070
Marcus Li <mmli@lcs.on.ca>
2025-01-09 15:03:41 -0500

    Initial Commit

---
+++ LottieAnimationList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LottieAnimationList
+//
+//  Created by Marcus Li on 2025-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LottieAnimationList/LottieAnimationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationListApp.swift
+//  LottieAnimationList
+//
+//  Created by Marcus Li on 2025-01-09.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
